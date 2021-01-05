a1d674c38b73267d1548e50e43fb13c58a0a9005
Romain Strock <romain.strock@gmail.com>
2021-01-05 08:35:02 +0000

    indent

--- src/dna/dna.cpp
+++ src/dna/dna.cpp
@@ -5,20 +5,20 @@ using namespace std;
 
 void ReverseComplement(string& seq) {
 	reverse(seq.begin(), seq.end());
-    for (size_t i = 0; i < seq.size(); ++i){
-        switch (seq[i]) {
-            case 'A':
-                seq[i] = 'T';
-                break;    
-            case 'C':
-                seq[i] = 'G';
-                break;
-            case 'G':
-                seq[i] = 'C';
-                break;
-            case 'T':
-                seq[i] = 'A';
-                break;
-            }
-    }
+	for (size_t i = 0; i < seq.size(); ++i){
+		switch (seq[i]) {
+			case 'A':
+				seq[i] = 'T';
+				break;    
+			case 'C':
+				seq[i] = 'G';
+				break;
+			case 'G':
+				seq[i] = 'C';
+				break;
+			case 'T':
+				seq[i] = 'A';
+				break;
+			}
+	}
 }
